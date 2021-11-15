0ab86daa92e24bd4644e2d99cbcead4c8b1aaf90
AtifAhmed29 <63032286+AtifAhmed29@users.noreply.github.com>
2021-11-16 00:42:27 +0600

    Initial commit

---
+++ Npuzzel.py
@@ -0,0 +1,31 @@
+
+
+matrix=[[6,1,10,2],[7,11,4,14],[5,0,5,15],[8,12,13,3]]
+inver=[]
+def inversion(arr):
+    inv_count = 0
+    empty_value = -1
+    for i in range(0, 4):
+        for j in range(i + 1, 4):
+            if arr[j] != empty_value and arr[i] != empty_value and arr[i] > arr[j]:
+                inv_count += 1
+    return inv_count
+    
+
+def isSolvable(puzzle) :
+ 
+    
+    inv_count = inversion([j for sub in puzzle for j in sub])
+ 
+   
+    return (inv_count % 2 == 0)
+    
+      
+if(isSolvable(matrix)) :
+    print("Solvable")
+else :
+    print("Not Solvable")
+
+
+    
+
